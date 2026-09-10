f7e77a3231b05c8f023ac87509aeb5496634ed33
Teufel2211 <210043657+Teufel2211@users.noreply.github.com>
2026-09-10 16:57:59 +0200

    Remove BOM from VersionCheckerClient.java

--- src/client/java/com/example/customservermod/version/VersionCheckerClient.java
+++ src/client/java/com/example/customservermod/version/VersionCheckerClient.java
@@ -1,4 +1,4 @@
-﻿package com.example.customservermod.version;
+package com.example.customservermod.version;
 
 import com.example.customservermod.CustomServerMod;
 import net.fabricmc.api.ClientModInitializer;
