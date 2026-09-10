6e0781db5ca163738305fb49d69f4d5a3e2e2d4e
Teufel2211 <210043657+Teufel2211@users.noreply.github.com>
2026-09-10 15:08:29 +0200

    Add PIZZA_ID (dev)

--- src/main/java/com/example/customservermod/CustomServerMod.java
+++ src/main/java/com/example/customservermod/CustomServerMod.java
@@ -15,6 +15,7 @@ public class CustomServerMod implements ModInitializer {
 	public static final Identifier EXCAVATION_ID = Identifier.fromNamespaceAndPath(MOD_ID, "excavation");
 	public static final Identifier AUTO_SMELTING_ID = Identifier.fromNamespaceAndPath(MOD_ID, "auto_smelting");
 	public static final Identifier INFINITE_TOTEM_ID = Identifier.fromNamespaceAndPath(MOD_ID, "infinite_totem");
+	public static final Identifier PIZZA_ID = Identifier.fromNamespaceAndPath(MOD_ID, "pizza");
 
 	@Override
 	public void onInitialize() {
